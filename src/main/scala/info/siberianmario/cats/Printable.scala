d31aecdf4dd1d048f36f3352a5cd372fe0d2f57d
Dmitry Andreev <siberian.mario@gmail.com>
2019-08-12 21:13:30 +0300

    Printable cat

--- src/main/scala/info/siberianmario/cats/Printable.scala
+++ src/main/scala/info/siberianmario/cats/Printable.scala
@@ -1,5 +1,8 @@
 package info.siberianmario.cats
 
+import info.siberianmario.cats.PrintableInstances._
+import info.siberianmario.cats.PrintableSyntax._
+
 trait Printable[A] {
   def format(a: A): String
 }
@@ -12,5 +15,30 @@ object Printable {
 
 object PrintableInstances {
   implicit val printableInt: Printable[Int] = (i: Int) => i.toString
-  implicit val printableString: Printable[String] = (i: String) => i
+  implicit val printableString: Printable[String] = (str: String) => str
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit p: Printable[A]): String = p.format(value)
+
+    def print(implicit p: Printable[A]): Unit = println(p.format(value))
+  }
+}
+
+final case class Cat(name: String, age: Int, color: String)
+
+object Cat {
+  implicit val printableCat: Printable[Cat] = (cat: Cat) => {
+    val name = Printable.format(cat.name)
+    val age = Printable.format(cat.age)
+    val color = Printable.format(cat.color)
+    s"$name is a $age year-old $color cat"
+  }
+}
+
+object PrintableApp extends App {
+  val pussy = Cat("Tom", 6, "black")
+  Printable.print(pussy)
+  pussy.print
 }
